a16f9b3a0e483e4c2261814ac35d217ac9bad8e4
Cacanny <dannyhoeve@gmail.com>
2017-11-15 11:42:08 +0100

    yes pull

--- EncryptionScheme/EncryptionScheme/Program.cs
+++ EncryptionScheme/EncryptionScheme/Program.cs
@@ -6,16 +6,34 @@ namespace EncryptionScheme
     {
         static void Main(string[] args)
         {
-            RsaEncryption();
+            // Variables for the RSA encryption bruteforce operation.
+            int N = 267;
+            int c = 158;
+            int e = 5;
+
+            BruteForceRsaEncryption(N, c, e);
         }
 
 
-        public static void RsaEncryption()
+        public static void BruteForceRsaEncryption(int N, int e, int c)
         {
             Console.WriteLine("################################################################");
             Console.WriteLine(" Waiting ...");
             Console.WriteLine("################################################################");
             Console.Read();
+
+            // calculate d. 
+
+        }
+
+        public static int[] ReturnPrime(int N)
+        {
+            int p;
+            int q;
+
+           int[] arrayWithPandQ = new int[10];
+
+            return arrayWithPandQ;
         }
 
     }
