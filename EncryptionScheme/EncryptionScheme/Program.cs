32e8f623cb2f081a43ae8d221e46c135569c5c59
Maarten Hulsman <maartenhulsman95@gmail.com>
2017-11-15 12:43:46 +0100

    rsa

--- EncryptionScheme/EncryptionScheme/Program.cs
+++ EncryptionScheme/EncryptionScheme/Program.cs
@@ -5,42 +5,72 @@ namespace EncryptionScheme
     class Program
     {
         public static int[] primesArray = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+        public static int p;
+        public static int q;
+        public static int d;
+        public static int N = 267;
+        public static int e = 5;
+        public static int c = 158;
 
         static void Main(string[] args)
         {
-            RsaEncryption(267, 5, 158);
+            
+            RsaEncryption();
         }
 
 
-        public static void RsaEncryption(int N, int e, int c)
+        public static void RsaEncryption()
         {
-            int[] pq = Primes(N);
-            foreach(int i in pq)
-            {
-                Console.WriteLine(i);
-
-            }
+            Primes(N);
+            CalculateD(e);
+            double message = Decrypt();
+            //Console.WriteLine(message.ToString());
             Console.Read();
         }
 
-        public static int[] Primes(int N)
+        public static void Primes(int N)
         {
-            int[] pq = new int[2];
-
             foreach(int i in primesArray)
             {
                 foreach(int j in primesArray)
                 {
                     if(i*j == N)
                     {
-                        pq[0] = i;
-                        pq[1] = j;
+                        p = i;
+                        q = j;
                         break;
                     }
                 }
             }
 
-            return pq;
+        }
+
+        public static void CalculateD(int e)
+        {
+            int pqMinus1 = (p - 1) * (q - 1);
+
+            for(int i=1; i<2000; i++)
+            {
+                if((e*i) % pqMinus1 == 1)
+                {
+                    d = i;
+                    Console.WriteLine("D: " + d);
+                    return;
+                }
+            }
+        }
+
+        public static double Decrypt()
+        {
+            Console.WriteLine("Decrypt");
+            double message = Math.Pow(c, d);
+            Console.WriteLine(message);
+        
+            double result = message % N;
+            Console.WriteLine(result);
+            return message;
+
+            
         }
     }
 }
