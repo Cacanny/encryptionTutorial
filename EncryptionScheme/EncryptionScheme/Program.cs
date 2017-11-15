884c6ba21a919bb832efbb0eb766bb036b8c17dd
Cacanny <dannyhoeve@gmail.com>
2017-11-15 14:48:44 +0100

    Tidying up

--- EncryptionScheme/EncryptionScheme/Program.cs
+++ EncryptionScheme/EncryptionScheme/Program.cs
@@ -1,76 +1,31 @@
 ﻿using System;
+using System.Numerics;
 
 namespace EncryptionScheme
 {
     class Program
     {
-        public static int[] primesArray = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
-        public static int p;
-        public static int q;
-        public static int d;
-        public static int N = 267;
-        public static int e = 5;
-        public static int c = 158;
 
         static void Main(string[] args)
         {
-            
-            RsaEncryption();
-        }
 
-
-        public static void RsaEncryption()
-        {
-            Primes(N);
-            CalculateD(e);
-            double message = Decrypt();
-            //Console.WriteLine(message.ToString());
-            Console.Read();
-        }
-
-        public static void Primes(int N)
-        {
-            foreach(int i in primesArray)
-            {
-                foreach(int j in primesArray)
-                {
-                    if(i*j == N)
-                    {
-                        p = i;
-                        q = j;
-                        break;
-                    }
-                }
-            }
+            // Static void function to start the RSA decryption;
+            StartRSADecryption();
 
         }
 
-        public static void CalculateD(int e)
+        public static void StartRSADecryption()
         {
-            int pqMinus1 = (p - 1) * (q - 1);
+            // Variables to send to new rsaEncryptor variable
+            int N = 267; // P * Q
+            int e = 5;  // Prime number
+            int c = 158; // The encrypted message
 
-            for(int i=1; i<2000; i++)
-            {
-                if((e*i) % pqMinus1 == 1)
-                {
-                    d = i;
-                    Console.WriteLine("D: " + d);
-                    return;
-                }
-            }
-        }
+            // Make a new instance of the RsaEncryptor with the above variables
+            RsaDecryptor rsaDecryptor = new RsaDecryptor(N, e, c);
 
-        public static double Decrypt()
-        {
-            Console.WriteLine("Decrypt");
-            double message = Math.Pow(c, d);
-            Console.WriteLine(message);
-        
-            double result = message % N;
-            Console.WriteLine(result);
-            return message;
-
-            
+            // Initialize a decryption of the message 
+            rsaDecryptor.RsaDecryption();
         }
     }
 }
