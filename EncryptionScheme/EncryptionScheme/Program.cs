ca4f51ff6038a566cf89850d5db38d5cf1490c61
Maarten Hulsman <maartenhulsman95@gmail.com>
2017-11-15 11:43:14 +0100

    commit

--- EncryptionScheme/EncryptionScheme/Program.cs
+++ EncryptionScheme/EncryptionScheme/Program.cs
@@ -6,34 +6,16 @@ namespace EncryptionScheme
     {
         static void Main(string[] args)
         {
-            // Variables for the RSA encryption bruteforce operation.
-            int N = 267;
-            int c = 158;
-            int e = 5;
-
-            BruteForceRsaEncryption(N, c, e);
+            RsaEncryption();
         }
 
 
-        public static void BruteForceRsaEncryption(int N, int e, int c)
+        public static void RsaEncryption()
         {
             Console.WriteLine("################################################################");
             Console.WriteLine(" Waiting ...");
             Console.WriteLine("################################################################");
             Console.Read();
-
-            // calculate d. 
-
-        }
-
-        public static int[] ReturnPrime(int N)
-        {
-            int p;
-            int q;
-
-           int[] arrayWithPandQ = new int[10];
-
-            return arrayWithPandQ;
         }
 
     }
