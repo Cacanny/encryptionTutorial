0d3a9efb2a356a776d274b2cfd7e97e12d4451d2
Cacanny <dannyhoeve@gmail.com>
2017-11-15 11:25:58 +0100

    add rsa enctrytpion function

--- EncryptionScheme/EncryptionScheme/Program.cs
+++ EncryptionScheme/EncryptionScheme/Program.cs
@@ -6,9 +6,17 @@ namespace EncryptionScheme
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RsaEncryption();
         }
 
+
+        public static void RsaEncryption()
+        {
+            Console.WriteLine("################################################################");
+            Console.WriteLine(" Waiting ...");
+            Console.WriteLine("################################################################");
+            Console.Read();
+        }
 
     }
 }
